923a345d1344006b8d048644658eac6213d73bba
lwqt99 <58200579+lwqt99@users.noreply.github.com>
2021-03-06 17:42:21 +0800

    Update detail.go

--- Go/sql/detail.go
+++ Go/sql/detail.go
@@ -6,6 +6,7 @@ type Detail struct{
 	Title string `json:"title" xorm:"unique not null varchar(255)"`
 	Word string `json:"word" xorm:"not null varchar(255)"`
 	Tags []string `json:"tags"`
+	Author string `json:"author" xorm:"not null"`
 	Address string `json:"adrress" xorm:"not null"`
 	Time time.Time `json:"time" xorm:"not null"`
 	DeleteTF bool `json:"deletetf" xorm:"not null"`
