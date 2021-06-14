d796f631df1c9096550f1498c3075d70af7e9742
lwqt99 <58200579+lwqt99@users.noreply.github.com>
2021-06-14 17:43:52 +0800

    Update huawei.go

--- BlockChain/sql/huawei.go
+++ BlockChain/sql/huawei.go
@@ -2,13 +2,13 @@ package sql
 
 type Huawei_fid_info struct{
 	Fid 		string `json:"fid" xorm:"pk unique"`
-	Uid			int `json:"uid" xorm:"not null int"`//学前标识符
+	Uid			string `json:"uid" xorm:"not null int(5)"`//学前标识符
 	Id 			string `json:"id" xorm:"varchar(20)"`//学号-待定是什么学号？大学的还是其他的？
 	Name 		string `json:"name" xorm:"not null varchar(20)"`//姓名
 	University	string `json:"university" xorm:"not null varchar(20)"`//学校
 	School		string `json:"school" xorm:"not null varchar(20)"`//学院
 	Course		string `json:"course" xorm:"not null varchar(20)"`//课程
-	Times		int `json:"times" xorm:"not null int "`//重修次数
+	Times		string `json:"times" xorm:"not null int "`//重修次数
 }
 
 type Huawei_evidence struct{
@@ -19,19 +19,18 @@ type Huawei_evidence struct{
 }
 
 type Huawei_grant2user struct{
-	Id			int		`json:"id" xorm:"int pk autoincr unique"`//编号
+	Id			string	`json:"id" xorm:"varchar(255) pk unique"`//编号
 	Name 		string	`json:"name" xorm:"not null varchar(20)"`//姓名
-	University	string	`json:"university" xorm:"not null varchar(20)"`//学校
-    Age 		string	`json:"age" xorm:"not null varchar(20)"`//年龄
-    Position 	string	`json:"position" xorm:"not null varchar(20)"`//职位
-    Gra 		int		`json:"gra" xorm:"not null int"`//授权true/false
+	Age 		string	`json:"age" xorm:"not null varchar(20)"`//年龄
+	Position 	string	`json:"position" xorm:"not null varchar(20)"`//职位
+	Gra 		int		`json:"gra" xorm:"not null int"`//授权true/false
 }
 
 type Huawei_grantedUser struct{
-	Id			int		`json:"id" xorm:"int pk autoincr unique"`//编号
+	Id			string	`json:"id" xorm:"varchar(255) pk unique"`//编号
 	Name 		string	`json:"name" xorm:"not null varchar(20)"`//姓名
-	University	string	`json:"university" xorm:"not null varchar(20)"`//学校
-    Age 		string	`json:"age" xorm:"not null varchar(20)"`//年龄
-    Position 	string	`json:"position" xorm:"not null varchar(20)"`//职位
-    Gra 		int		`json:"gra" xorm:"not null int"`//授权true/false
+	Age 		string	`json:"age" xorm:"not null varchar(20)"`//年龄
+	Pwd			string	`json:"pwd" xorm:"not null varchar(20)"`
+	Position 	string	`json:"position" xorm:"not null varchar(20)"`//职位
+	Gra 		int		`json:"gra" xorm:"not null int"`//授权true/false
 }
